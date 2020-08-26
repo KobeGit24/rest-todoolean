bbf1caa51c82c98f766811e5e43c2648b837922a
Stefano <stefano.zaccaria29@gmail.com>
2020-08-26 13:50:58 +0200

    ex complete!

--- script.js
+++ script.js
@@ -1,17 +1,77 @@
 function init() {
     getTodoList(); 
+    addItemTodoList();
+    deleteItem();
 }
 
 function getTodoList() {
+
     $.ajax({
         url: `http://157.230.17.132:3034/todos`,
         method: 'GET',
         success: function (data) {
-            console.log('data', data);
+            printTodoList(data);
         },
         error: function (err) {
             console.log('err', err);
         }
+    });
+}
+
+function printTodoList(item) {
+    
+    var list = $('#list');
+    list.text('');
+    for (var i = 0; i < item.length; i++) {
+        var y = item[i]; 
+        list.append(`<li> ${y.text} <span data-id= "${y.id}"  class="delete"><strong> X </strong></span></li>`);   
+    }
+}
+
+function addItemTodoList () {
+    
+    var btn = $('#add');
+    btn.click(insertItemApi);
+}
+
+function insertItemApi () {
+
+    var input = $('#new-item');
+    var text = input.val();
+
+    $.ajax({
+        url: `http://157.230.17.132:3034/todos`,
+        method: 'POST',
+        data: {
+            text: text
+        },
+        success: function (data) {
+            console.log('data', data);
+            getTodoList();
+        },
+        error: function (err) {
+            console.log('err', err);
+        }
+    });
+}
+
+function deleteItem() {
+    $(document).on('click','.delete', function () {
+       
+        var x = $(this); 
+        var z = x.data('id');
+
+        $.ajax({
+            url: `http://157.230.17.132:3034/todos/${z}`,
+            method: 'DELETE',
+            success: function (data) {
+                console.log('data', data);
+                getTodoList();
+            },
+            error: function (err) {
+                console.log('err', err);
+            }
+        });
     });
 }
 
